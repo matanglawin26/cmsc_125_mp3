45a655c7f3e84e817f86e9d291d5853236c7dcaa
Din Gilig <dinniel.gilig@module-zero.com>
2023-06-01 13:56:30 +0800

    updated code

--- mp3.py
+++ mp3.py
@@ -8,6 +8,9 @@ class Job:
         self._size = size
         self._waiting_time = 0
 
+    def increment(self):
+        self._waiting_time += 1
+
     def decrement(self):
         self._time -= 1
 
@@ -18,7 +21,7 @@ class Job:
         self._waiting_time = waiting_time
 
     def __repr__(self):
-        return "Job %d: Time: %d Size: %d" % (self._id, self._time, self._size)
+        return "Job %d: Time: %d Size: %d Waiting Time: %d" % (self._id, self._time, self._size, self._waiting_time)
 
 
 class Memory:
@@ -27,6 +30,7 @@ class Memory:
         self._size = size
         self._upper_limit = 0
         self._occupied_by = None
+        self._used = 0
 
     def get_int_frag(self):
         if not self.is_available():
@@ -63,40 +67,82 @@ class Clock:
 class Metrics:
     def __init__(self):
         self._throughput = []
+        self._mem_used = []
         self._mem_never_used = []
         self._mem_heavily_used = []
         self._int_frag = []
-        self._queue_length = 0
-        self._time_in_queue = 0
+        self._waiting_time = []
+        self._queue_length = []
         self._total_mem = 10
 
     def add_throughput(self, curr_mem_list):
         self._throughput.append(
             sum(1 if not mem.is_available() else 0 for mem in curr_mem_list))
 
+    def add_mem_used(self, curr_mem_list):
+        self._mem_used.append(
+            round((sum(1 if not mem.is_available() else 0 for mem in curr_mem_list)/self._total_mem) * 100, 2))
+
     def add_mem_never_used(self, curr_mem_list):
         self._mem_never_used.append(
-            sum(1 if mem.is_available() else 0 for mem in curr_mem_list))
+            round((sum(1 if mem.is_available() else 0 for mem in curr_mem_list)/self._total_mem) * 100, 2))
 
     def add_mem_heavily_used(self, curr_mem_list):
         self._mem_heavily_used.append(
-            sum(1 if not mem.is_available() else 0 for mem in curr_mem_list))
+            sum(1 if mem._used > 1 else 0 for mem in curr_mem_list))
 
     def add_int_frag(self, curr_mem_list):
         self._int_frag.append(
             sum(mem.get_int_frag() for mem in curr_mem_list))
 
+    def add_waiting_time(self, curr_job_list):
+        self._waiting_time.append(
+            sum(job._waiting_time for job in curr_job_list))
+
+    def add_queue_length(self, q_length):
+        self._queue_length.append(q_length)
+
     def get_curr_throughput(self):
         return self._throughput[-1]
 
-    def get_mem_never_used(self):
-        return "%.1f%%" % ((self._mem_never_used[-1]/self._total_mem) * 100)
+    def get_curr_mem_used(self):
+        return "%.1f%%" % self._mem_used[-1]
 
-    def get_mem_heavily_used(self):
-        return self._mem_heavily_used[-1]
+    def get_curr_mem_never_used(self):
+        return "%.1f%%" % self._mem_never_used[-1]
 
-    def get_int_frag(self):
+    def get_curr_mem_heavily_used(self):
+        return "%.1f%%" % self._mem_heavily_used[-1]
+
+    def get_curr_int_frag(self):
         return self._int_frag[-1]
+
+    def get_curr_queue_length(self):
+        return self._queue_length[-1]
+
+    def get_waiting_time(self):
+        return self._waiting_time[-1]
+
+    def get_avg_throughput(self):
+        return sum(self._throughput)/len(self._throughput)
+
+    def get_avg_mem_used(self):
+        return sum(self._mem_used)/len(self._mem_used)
+
+    def get_avg_mem_never_used(self):
+        return sum(self._mem_never_used)/len(self._mem_never_used)
+
+    def get_avg_mem_heavily_used(self):
+        return sum(self._mem_heavily_used)/len(self._mem_heavily_used)
+
+    def get_avg_queue_length(self):
+        return sum(self._queue_length)/len(self._queue_length)
+
+    def get_avg_int_frag(self):
+        return sum(self._int_frag)/len(self._int_frag)
+
+    def get_waiting_time(self):
+        return sum(self._waiting_time)/len(self._waiting_time)
 
 
 class System:
@@ -112,12 +158,12 @@ class System:
         idx = 0
         while idx < len(self._mem_list):
             mem = self._mem_list[idx]
-        # for mem in self._mem_list:
             if not self.is_memory_full():
                 if self.is_mem_allocatable(mem) and mem.is_available():
                     job = self.get_top()
                     if job._size <= mem._size:
                         mem.set_occupied(job)
+                        mem._used += 1
                         idx += 1
                         self._queue.sort(key=lambda job: job._id)
                     else:
@@ -131,10 +177,13 @@ class System:
         while self.is_occupied() or self.is_jobs_allocatable():
             cprint("\nTime Elapsed: %ds\n" % self._clock._tick, "light_green")
 
+            self._metrics.add_mem_used(self._mem_list)
             self._metrics.add_mem_never_used(self._mem_list)
+            self._metrics.add_mem_heavily_used(self._mem_list)
             self._metrics.add_throughput(self._mem_list)
-            self._metrics.add_mem_never_used(self._mem_list)
             self._metrics.add_int_frag(self._mem_list)
+            self._metrics.add_waiting_time(self.get_valid_queue())
+            self._metrics.add_queue_length(len(self.get_valid_queue()))
             self.status()
 
             for mem in self._mem_list:
@@ -145,8 +194,9 @@ class System:
                     else:
                         mem.decrement()
 
+            self.increase_waiting_time()
             self._clock.up()
-            input()
+            # input()
 
     def is_mem_allocatable(self, curr_mem):
         for job in self._queue:
@@ -166,29 +216,30 @@ class System:
                 return True
         return False
 
-    def get_top(self):
-        if len(self._queue) > 0:
-            return self._queue.pop(0)
-        else:
-            return None
-
-    def get_queue_length(self):
-        return sum(1 if self.is_valid_job(job) else 0 for job in self._queue)
-
     def is_occupied(self):
         for mem in self._mem_list:
             if not mem.is_available():
                 return True
         return False
 
-    def is_empty(self):
-        return len(self._queue) == 0
-
     def is_memory_full(self):
         for mem in self._mem_list:
             if mem.is_available():
                 return False
         return True
+
+    def get_top(self):
+        if len(self._queue) > 0:
+            return self._queue.pop(0)
+        else:
+            return None
+
+    def get_valid_queue(self):
+        return [job for job in self._queue if self.is_valid_job(job)]
+
+    def increase_waiting_time(self):
+        for job in self._queue:
+            job.increment()
 
     def configure_limit(self):
         curr_sum = 0
@@ -204,45 +255,53 @@ class System:
         cprint("Throughput:", "light_magenta", end=' ')
         print(self._metrics.get_curr_throughput())
         cprint("Storage utilization:", "light_magenta")
+
+        cprint('{:>34}'.format("Percentage of Partitions Used:"
+                               ), "dark_grey", end=' ')
+        print(self._metrics.get_curr_mem_used())
+
         cprint('{:>40}'.format("Percentage of Partitions Never Used:"
                                ), "dark_grey", end=' ')
-        print(self._metrics.get_mem_never_used())
+        print(self._metrics.get_curr_mem_never_used())
 
-        cprint('{:>42}'.format("Percentage of Partitions Heavily Used:"
-                               ), "dark_grey", end=' ')
-        print(None)  # Change this
+        # cprint('{:>42}'.format("Percentage of Partitions Heavily Used:"
+        #                        ), "dark_grey", end=' ')
+        # print(self._metrics.get_curr_mem_heavily_used())  # Change this
 
         cprint("Waiting Queue Length:", "light_magenta", end=' ')
-        print(self.get_queue_length())
+        print(self._metrics.get_curr_queue_length())
 
         cprint("Waiting Time in Queue:", "light_magenta", end=' ')
-        print(None)  # Change this
+        print("%ds" % self._metrics.get_waiting_time())
 
-        cprint("Total Internal Fragmentation:", "light_magenta", end=' ')
-        print(self._metrics.get_int_frag())
+        cprint("Internal Fragmentation:", "light_magenta", end=' ')
+        print(self._metrics.get_curr_int_frag())
 
     def display_chart(self):
         mem_list = sorted(self._mem_list, key=lambda mem: mem._id)
-        print("{:<18}".format(""), end='')
+        print("{:<25}".format(""), end='')
         print("┌──────────────────────┬── 0")
         for (idx, mem) in enumerate(mem_list):
             if mem.is_available():
-                print("{:^18}".format("Memory Block %d" % mem._id), end='')
+                print("{:^25}".format("Memory Block %d (%d)" %
+                      (mem._id, mem._size)), end='')
                 print("│ {:^20} │".format("No Job Assigned"))
             else:
                 int_frag = mem.get_int_frag()
-                print("{:<18}".format(""), end='')
+                print("{:<25}".format(""), end='')
                 print("│ {:^20} │".format(
                     "Job %d (%ds)" % (mem._occupied_by._id,  mem._occupied_by._time)))
 
-                print("{:^18}".format("Memory Block %d" % mem._id), end='')
-                print("│ {:^20} │".format("Size: %d" % mem._size))
+                print("{:^25}".format("Memory Block %d (%d)" %
+                      (mem._id, mem._size)), end='')
+                print("│ {:^20} │".format(
+                    "Size: %d" % mem._occupied_by._size))
 
-                print("{:<18}".format(""), end='')
+                print("{:<25}".format(""), end='')
                 print("│ {:^20} │".format(
                     "Int. Frag.: %d" % int_frag))
 
-            print("{:<18}".format(""), end='')
+            print("{:<25}".format(""), end='')
             if idx == len(mem_list) - 1:
                 print(
                     "└──────────────────────┴──", mem._upper_limit)
@@ -251,7 +310,48 @@ class System:
                     "├──────────────────────┼──", mem._upper_limit)
 
     def final_metrics(self):
-        print("Time Elapsed: %ds" % self._clock._tick)
+        cprint("\nTime Elapsed: %ds\n" % self._clock._tick, "light_green")
+
+        cprint("Final Metrics\n", "light_yellow")
+
+        # Total Data
+        cprint("Total:", "light_blue")
+
+        cprint("Waiting Queue Length:", "light_magenta", end=' ')
+        print("%d jobs" % sum(self._metrics._queue_length))
+
+        cprint("Waiting Time in Queue:", "light_magenta", end=' ')
+        print("%ds" % sum(self._metrics._waiting_time))
+
+        cprint("Internal Fragmentation:", "light_magenta", end=' ')
+        print("%d units" % sum(self._metrics._int_frag))
+
+        # Average Data
+        cprint("\nAverage:", "light_blue")
+        cprint("Throughput:", "light_magenta", end=' ')
+        print("%.2f jobs/s" % self._metrics.get_avg_throughput())
+        cprint("Storage utilization:", "light_magenta")
+
+        cprint('{:>34}'.format("Percentage of Partitions Used:"
+                               ), "dark_grey", end=' ')
+        print("%.2f%%" % self._metrics.get_avg_mem_used())
+
+        cprint('{:>40}'.format("Percentage of Partitions Never Used:"
+                               ), "dark_grey", end=' ')
+        print("%.2f%%" % self._metrics.get_avg_mem_never_used())
+
+        cprint('{:>42}'.format("Percentage of Partitions Heavily Used:"
+                               ), "dark_grey", end=' ')
+        print("%.2f%%" % self._metrics.get_avg_mem_heavily_used())
+
+        cprint("Waiting Queue Length:", "light_magenta", end=' ')
+        print("%.2f jobs" % self._metrics.get_avg_queue_length())
+
+        cprint("Waiting Time in Queue:", "light_magenta", end=' ')
+        print("%ds" % self._metrics.get_waiting_time())
+
+        cprint("Internal Fragmentation:", "light_magenta", end=' ')
+        print("%.2f units" % self._metrics.get_avg_int_frag())
 
 
 class FirstFit(System):
@@ -287,15 +387,15 @@ def main():
     job_list = [list(map(int, job.split())) for job in job_file]
     mem_list = [list(map(int, mem.split())) for mem in mem_file]
 
-    first_fit = FirstFit(job_list, mem_list)
-    # best_fit = BestFit(job_list, mem_list)
-    worst_fit = WorstFit(job_list, mem_list)
+    # first_fit = FirstFit(job_list, mem_list)
+    best_fit = BestFit(job_list, mem_list)
+    # worst_fit = WorstFit(job_list, mem_list)
 
-    first_fit.run()
-    first_fit.final_metrics()
+    # first_fit.run()
+    # first_fit.final_metrics()
     # print()
-    # best_fit.run()
-    # best_fit.final_metrics()
+    best_fit.run()
+    best_fit.final_metrics()
     # print()
     # worst_fit.run()
     # worst_fit.final_metrics()
